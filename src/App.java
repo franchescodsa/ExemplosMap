5dc1c183cfecdaf12d98d1abf0538917f3d95029
franchesco aprigio <franchescodsa@outlook.com>
2024-10-20 20:23:37 -0300

    demo 2

--- src/App.java
+++ src/App.java
@@ -1,25 +1,23 @@
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
+import entities.Product;
 
 public class App {
-    public static void main(String[] args)  {
-         Map<String, String> cookies = new TreeMap<>();
- cookies.put("username", "maria");
- cookies.put("email", "maria@gmail.com");
- cookies.put("phone", "99771122");
+    public static void main(String[] args) {
 
- cookies.remove("email");
+        Map<Product, Double> stock = new HashMap<>();
 
- cookies.put("phone", "99771133");
- 
- System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
- System.out.println("Phone number: " + cookies.get("phone"));
- System.out.println("Email: " + cookies.get("email"));
- System.out.println("Size: " + cookies.size());
+        Product p1 = new Product("Tv", 900.0);
+        Product p2 = new Product("Notebook", 1200.0);
+        Product p3 = new Product("Tablet", 400.0);
 
-    System.out.println("ALL COOKIES:");
-    for (String key : cookies.keySet()) {
- System.out.println(key + ": " + cookies.get(key));
- }
+        stock.put(p1, 10000.0);
+        stock.put(p2, 20000.0);
+        stock.put(p3, 15000.0);
+
+        Product ps = new Product("Tv", 900.0);
+
+        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
     }
 }
